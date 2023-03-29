2f2a654c861a0087f809df6744e955d7edd46939
SunCanyang <try028@163.com>
2023-03-29 14:50:32 +0800

    更新Singleton

--- applications/odata/src/main/java/com/dpbird/odata/SingletonHandler.java
+++ applications/odata/src/main/java/com/dpbird/odata/SingletonHandler.java
@@ -1,19 +1,32 @@
 package com.dpbird.odata;
 
+import org.apache.ofbiz.base.util.UtilMisc;
+import org.apache.ofbiz.base.util.UtilValidate;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.GenericEntityException;
 import org.apache.ofbiz.entity.GenericValue;
 import org.apache.ofbiz.entity.util.EntityQuery;
+import org.apache.ofbiz.entity.util.EntityUtil;
 import org.apache.olingo.commons.api.ex.ODataException;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 import java.util.Map;
 
 public class SingletonHandler {
     public static Object getMe(HttpSession httpSession, Map<String, Object> singletonContext)
             throws ODataException {
-        return singletonContext.get("userLogin");
-
+        Delegator delegator = (Delegator) singletonContext.get("delegator");
+        GenericValue userLogin = (GenericValue) singletonContext.get("userLogin");
+        try {
+            if (UtilValidate.isNotEmpty(userLogin)) {
+                List<GenericValue> partyUserLogin = delegator.findByAnd("PartyUserLogin", userLogin.getPrimaryKey(), null, true);
+                return EntityUtil.getFirst(partyUserLogin);
+            }
+            return null;
+        } catch (GenericEntityException e) {
+            throw new OfbizODataException(e.getMessage());
+        }
     }
     public static Object productSingleton(HttpSession httpSession, Map<String, Object> singletonContext)
             throws ODataException, GenericEntityException {
