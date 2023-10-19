c4e72eb0dec8c7cd96eebc450eabf481a45aaa5a
SunCanyang <try028@163.com>
2023-10-19 16:57:59 +0800

    更新在线配置

--- applications/odata/src/main/java/com/dpbird/odata/OfbizAppEdmProvider.java
+++ applications/odata/src/main/java/com/dpbird/odata/OfbizAppEdmProvider.java
@@ -427,8 +427,12 @@ public class OfbizAppEdmProvider extends CsdlAbstractEdmProvider {
             } else {
                 GenericValue edmAppConfig = EntityQuery.use(delegator).from("EdmService").where("serviceName", webapp).queryFirst();
                 if (edmAppConfig != null) {
-                    String edmConfigContent = edmAppConfig.getString("edmContent");
-                    return new ByteArrayInputStream(edmConfigContent.getBytes(StandardCharsets.UTF_8));
+                    GenericValue edmContent = EntityQuery.use(delegator).from("EdmServiceContent")
+                            .where("edmServiceId", edmAppConfig.getString("edmServiceId"), "format", "xml").queryFirst();
+                    if (UtilValidate.isNotEmpty(edmContent)) {
+                        String edmConfigContent = edmContent.getString("edmContent");
+                        return new ByteArrayInputStream(edmConfigContent.getBytes(StandardCharsets.UTF_8));
+                    }
                 }
             }
         } catch (FileNotFoundException | MalformedURLException | GenericEntityException e) {
